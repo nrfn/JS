6fd84cc04b5c2d9efa90478282860fd399953b87
nrfn <59220732+nrfn@users.noreply.github.com>
2024-07-11 00:48:37 +0900

    self-introduction.js

---
+++ Exercise/self-introduction.js
@@ -0,0 +1,6 @@
+let named = "nrfn";
+let language = "JavaScript";
+let hitokoto = "freelance";
+console.log("name: " + named);
+console.log("language: " + language);
+console.log("hitokoto: " + hitokoto);
